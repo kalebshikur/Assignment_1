11d38c7ab254d1e2203a427af3894289a9d9de95
kalebshikur <shikurkm@mymail.vcu.edu>
2018-09-22 14:48:38 -0400

    name change

---
+++ Project7/Assignement_1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+	int amount;
+	int i, j = 0;
+	int bills[]{ 50,20,10,5,1 }; // creates array for the bills the atm can withdraw
+	int withDraw;
+	char entry;
+	do // this loop is used to give the users multiple chance to withdraw
+	{
+		while (true) // This loop checks if the user is entering a valid input
+		{
+			cout << "Enter withdrawal amount: " << endl;
+			cin >> amount;
+	// check the top limit of the entry
+			if (amount > 300) { 
+				cout << " Sorry the maximum amount you can withdral is $300" << endl;
+			}
+	// check the bottom limit of the entry
+			else if (amount < 1) {
+				cout << " Sorry the minimum amount you can withdraw if $1" << endl;
+			}
+			else
+
+	// this loops count the amount of bills withdrawn
+			{
+	/* the loops subtracts the largest bill that is smaller than the amount entered 
+	until the amount goes to zero or the amount is smaller than the bill and it will
+	go down to the next smaller bill */
+				for (i = 0; i < 5; i++)
+				{
+					j = 0;
+					if (amount >= bills[i]) {
+						do
+						{
+							amount = amount - bills[i];
+							j++;
+						} while (amount >= bills[i]);
+						cout << j << " $" << bills[i] << " bill" << endl;
+					}
+				}
+	// Ask if the user wants to enter another withdrawal
+				cout << "Another withdrawal?  please enter y to continue" << endl;
+				cin >> entry;
+	// if user enters anything other than 'y' the program will terminate
+				break;
+			}
+		}
+	// check the entry of the user if the users wants to enter another withdrawal
+	} while (entry == 'y'|| entry == 'Y');
+}
